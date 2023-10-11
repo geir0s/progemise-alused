412ff744ea8deaaf854933b4068a3255a9668cbb
Geir Oss <geiross@Geirs-MacBook-Air.local>
2023-10-11 15:51:31 +0300

    first commit

---
+++ checkTemp.js
@@ -0,0 +1,22 @@
+temp = prompt('Mitu kraadi on vesi?')
+let liiga_külm = -1
+let paras_temperatuur = 0
+let liiga_kuum = 1
+
+function checkTemp() {
+    if (temp < 20) {
+        console.log(liiga_külm)
+    }
+    else if (temp > 20 && temp <= 40) {
+        console.log(paras_temperatuur)
+    } 
+    else if (temp > 40 && temp <= 60) {
+        console.log(paras_temperatuur)
+    }
+    
+}
+
+checkTemp()   
+
+
+
