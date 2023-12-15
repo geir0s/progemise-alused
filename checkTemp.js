c0d999fc8e0eb0584072da98fd160d311b65676e
geiros <146537333+geir0s@users.noreply.github.com>
2023-12-15 08:47:40 +0200

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,22 +1,25 @@
-temp = prompt('Mitu kraadi on vesi?')
-let liiga_külm = -1
-let paras_temperatuur = 0
-let liiga_kuum = 1
+let temp = prompt('Mitu kraadi on vesi?');
+let liiga_külm = -1;
+let paras_temperatuur = 0;
+let liiga_kuum = 1;
 
 function checkTemp() {
+    temp = parseFloat(temp); // Input floatiks
+
     if (temp < 20) {
-        console.log(liiga_külm)
+        console.log("liiga_külm");
+        return "liiga_külm"; // Tagastab string value
+    } else if (temp >= 20 && temp <= 40) {
+        console.log("paras_temperatuur");
+        return "paras_temperatuur"; // Tagastab string value
+    } else if (temp > 40 && temp <= 60) {
+        console.log("liiga_kuum");
+        return "liiga_kuum"; // Tagastab string value
+    } else {
+        console.log("Invalid temperature");
+        return "Invalid temperature"; // Error kui temp out of range
     }
-    else if (temp > 20 && temp <= 40) {
-        console.log(paras_temperatuur)
-    } 
-    else if (temp > 40 && temp <= 60) {
-        console.log(liiga_kuum)
-    }
-    
 }
 
-checkTemp()   
-
-
-
+let result = checkTemp();
+console.log(result); // Results
